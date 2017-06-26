c49e99e8a6f8f192c6ed1b9f33237865bd5a6978
alexsevv <aaa123qqq@mail.ru>
2017-06-26 11:58:22 +0300

    sugar

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,6 @@ class Event < ApplicationRecord
   has_many :photos
 
   validates :user, presence: true
-
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
   validates :datetime, presence: true
